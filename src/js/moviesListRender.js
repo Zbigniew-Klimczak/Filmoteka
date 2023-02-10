fdf1709637f30ddb51930524bdfe4413fe3e1913
Pawel Gorniak <p_gorniak@tlen.pl>
2023-02-10 12:46:46 +0100

    CSS for movies list + JS update for render of movies list

--- src/js/moviesListRender.js
+++ src/js/moviesListRender.js
@@ -1,6 +1,6 @@
 import { genresIDs } from './genresCodesFetch';
 
-const moviesList = document.querySelector('.movies__container');
+const moviesList = document.querySelector('.movies__list');
 
 export function moviesListRender(movie) {
   const movieGenres = [];
@@ -15,14 +15,16 @@ export function moviesListRender(movie) {
 
   moviesList.insertAdjacentHTML(
     'beforeend',
-    `<div class="movie">
-        <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" 
+    `<li class="movie" data-id="${movie.id}">
+        <img class="movie__img" 
+        src="https://image.tmdb.org/t/p/w500${movie.poster_path}" 
         alt="${movie.title} poster." loading="lazy" />
         <h2 class="movie__title">${movie.title}</h2>
         <div class="movie__info">
-            <p class="movie__detail">${movieGenres.join(', ')}</p>
+            <p class="movie__detail">${movieGenres.slice(1, 5).join(', ')}</p>
+            <p class="movie__detail">|</p>
             <p class="movie__detail">${movie.release_date.slice(0, 4)}</>
         </div>
-    </div>`
+    </li>`
   );
 }
