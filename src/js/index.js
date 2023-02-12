eebdf171280acd02cde8ff5756b48f3760128e3c
Pawel Gorniak <p_gorniak@tlen.pl>
2023-02-12 20:16:28 +0100

    Refactor fetch code + Create search function

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,29 @@
-import { getTrendingMovies } from './trendingMoviesFetch';
-import { getGenresIDs } from './genresCodesFetch';
+import { API_KEY } from './apiKey';
+import { fetchJsonResponse } from './responseJsonFetch';
 import { moviesListRender } from './moviesListRender';
 
-window.onload = () => {
-  getGenresIDs();
+const searchBtn = document.querySelector('.search__button');
+const searchInput = document.querySelector('.search__input');
 
-  getTrendingMovies().then(movies => {
-    const moviesData = movies.results;
-    moviesData.forEach(movie => {
-      moviesListRender(movie);
-    });
+window.onload = () => {
+  fetchJsonResponse('https://api.themoviedb.org/3/trending/movie/day', {
+    api_key: API_KEY,
+  }).then(response => {
+    moviesListRender(response.results);
   });
 };
+
+searchBtn.addEventListener('click', event => {
+  event.preventDefault();
+  fetchJsonResponse('https://api.themoviedb.org/3/search/movie', {
+    api_key: API_KEY,
+    language: 'en-US',
+    query: searchInput.value,
+    include_adult: false,
+  }).then(response => {
+    moviesListRender(response.results);
+  });
+  if (searchInput.value != '') {
+    searchInput.value = '';
+  }
+});
