185e85ddd04db6a31f6aaf8a97b06872092138e4
Zbigniew-Klimczak <111083175+Zbigniew-Klimczak@users.noreply.github.com>
2023-02-20 13:41:47 +0100

    many bugfixes

--- src/js/footerModal.js
+++ src/js/footerModal.js
@@ -3,6 +3,7 @@
     openModalBtn: document.querySelector('.copyright__span'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
+    body: document.querySelector('body'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
@@ -10,5 +11,6 @@
 
   function toggleModal() {
     refs.modal.classList.toggle('modal__hidden');
+    refs.body.classList.toggle('noScroll');
   }
 })();
