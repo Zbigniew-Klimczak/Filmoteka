7be6ce7e03902413e3e21661af53a12950dd59e8
Zbigniew-Klimczak <111083175+Zbigniew-Klimczak@users.noreply.github.com>
2023-02-20 21:10:48 +0100

    loading fixes

--- src/js/library.js
+++ src/js/library.js
@@ -8,49 +8,57 @@ const moviesList = document.querySelector('.movies__list');
 const watchedBtn = document.querySelector('.watched-button');
 const queueBtn = document.querySelector('.queue-button');
 
+const queueLength = JSON.parse(localStorage.getItem('movies-queue')).length;
+const watchedLength = JSON.parse(localStorage.getItem('movies-watched')).length;
+
 let fetchLibrary = () => {
-  trackMousePosition();
   moviesList.innerHTML = '';
   queueBtn.classList.remove('queue-button--active');
   watchedBtn.classList.add('watched-button--active');
-  let parsedLibrary = JSON.parse(localStorage.getItem('movies-watched'));
-  parsedLibrary.forEach(async id => {
-    fetchJsonResponse(`https://api.themoviedb.org/3/movie/${id}`, {
-      api_key: API_KEY,
-    }).then(movie => {
-      let genres = movie.genres;
-      let movieGenres = [];
-      for (let i = 0; i < genres.length; i++) {
-        movieGenres.push(genres[i].name);
-      }
-      displayMovieCard(movie, movieGenres);
+  if (watchedLength !== 0) {
+    trackMousePosition();
+    let parsedLibrary = JSON.parse(localStorage.getItem('movies-watched'));
+    parsedLibrary.forEach(async id => {
+      fetchJsonResponse(`https://api.themoviedb.org/3/movie/${id}`, {
+        api_key: API_KEY,
+      }).then(movie => {
+        let genres = movie.genres;
+        let movieGenres = [];
+        for (let i = 0; i < genres.length; i++) {
+          movieGenres.push(genres[i].name);
+        }
+        displayMovieCard(movie, movieGenres);
+      });
     });
-  });
-  stopTrackingMousePosition();
+    stopTrackingMousePosition();
+  }
 };
 
 let fetchQueue = () => {
-  trackMousePosition();
   moviesList.innerHTML = '';
   watchedBtn.classList.remove('watched-button--active');
   queueBtn.classList.add('queue-button--active');
-  let parsedLibrary = JSON.parse(localStorage.getItem('movies-queue'));
-  parsedLibrary.forEach(async id => {
-    fetchJsonResponse(`https://api.themoviedb.org/3/movie/${id}`, {
-      api_key: API_KEY,
-    }).then(movie => {
-      let genres = movie.genres;
-      let movieGenres = [];
-      for (let i = 0; i < genres.length; i++) {
-        movieGenres.push(genres[i].name);
-      }
-      displayMovieCard(movie, movieGenres);
+  if (queueLength !== 0) {
+    trackMousePosition();
+    let parsedLibrary = JSON.parse(localStorage.getItem('movies-queue'));
+    parsedLibrary.forEach(async id => {
+      fetchJsonResponse(`https://api.themoviedb.org/3/movie/${id}`, {
+        api_key: API_KEY,
+      }).then(movie => {
+        let genres = movie.genres;
+        let movieGenres = [];
+        for (let i = 0; i < genres.length; i++) {
+          movieGenres.push(genres[i].name);
+        }
+        displayMovieCard(movie, movieGenres);
+      });
     });
-  });
-  stopTrackingMousePosition();
+    stopTrackingMousePosition();
+  }
 };
 
 fetchLibrary();
+
 queueBtn.addEventListener('click', fetchQueue);
 watchedBtn.addEventListener('click', fetchLibrary);
 moviesList.addEventListener('click', modalMovie);
