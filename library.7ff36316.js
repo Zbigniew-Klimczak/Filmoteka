ebcf34d2cc113d068b737472e8100814ebffb079
Zbigniew-Klimczak <111083175+Zbigniew-Klimczak@users.noreply.github.com>
2023-02-20 12:58:21 +0000

    Deploying to gh-pages from @ Zbigniew-Klimczak/projekt-grupa-1@fc828c214b8f928cd07740648a5f2316cd717178 🚀

---
+++ library.7ff36316.js
@@ -0,0 +1,2 @@
+!function(){var o={openModalBtn:document.querySelector(".copyright__span"),closeModalBtn:document.querySelector("[data-modal-close]"),modal:document.querySelector("[data-modal]"),body:document.querySelector("body")};function e(){o.modal.classList.toggle("modal__hidden"),o.body.classList.toggle("noScroll")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)}();
+//# sourceMappingURL=library.7ff36316.js.map
--- library.b081a108.js
+++
@@ -1,2 +0,0 @@
-!function(){var e={openModalBtn:document.querySelector(".copyright__span"),closeModalBtn:document.querySelector("[data-modal-close]"),modal:document.querySelector("[data-modal]")};function o(){e.modal.classList.toggle("modal__hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
-//# sourceMappingURL=library.b081a108.js.map
